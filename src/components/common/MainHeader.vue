9ed964f8d53f46c7573cb24fcf7865215e4f780d
KimJangsoon <ssnssnl@kakao.com>
2023-02-28 17:56:30 +0900

    refactor : [김장순] 메인페이지 헤더 UI (+UI수정) [mung-front2]

--- src/components/common/MainHeader.vue
+++ src/components/common/MainHeader.vue
@@ -1,11 +1,11 @@
 <template>
-  <div class="text-black flex items-center justify-between bg-white ml-20 mr-20 mt-4">
+  <div class="text-black flex items-center justify-between bg-white ml-20 mr-20 mt-4 mb-4">
     <router-link to="/" class="text-cl"> MungMung </router-link>
     <div class="flex items-center">
       <router-link to="/dog-wiki" class="text-lg mr-10 text-black hover:text-indigo-500 hover:bg-gray-100 rounded-lg">
         멍멍위키
       </router-link>
-      <router-link to="/dog-walking" class="text-lg mr-10 text-black hover:text-indigo-500">산책인증</router-link>
+      <router-link to="/walking-dog" class="text-lg mr-10 text-black hover:text-indigo-500">산책인증</router-link>
       <router-link to="/dog-column" class="text-lg mr-10 text-black hover:text-indigo-500">애견컬럼</router-link>
       <img v-if="isSearchVisible" class="animate-fade-in-down w-5 mr-10" src="@/assets/logo/search_icon.png" />
       <router-link to="/login" class="px-4 py-2 rounded-lg text-white bg-indigo-500 hover:bg-indigo-600"
@@ -20,7 +20,7 @@ export default {
   name: 'MainHeader',
   computed: {
     isSearchVisible() {
-      return ['/dog-wiki', '/dog-walking', '/dog-column'].includes(this.$route.path);
+      return ['/dog-wiki', '/walking-dog', '/dog-column'].includes(this.$route.path);
     },
   },
 };
